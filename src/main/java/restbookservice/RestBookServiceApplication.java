b34b156123cb09f02fff462f1f9671534e3007db
Vadim Khizhin <martin.faulther@yandex.ru>
2020-11-23 15:29:47 +0300

    Initial commit

---
+++ src/main/java/restbookservice/RestBookServiceApplication.java
@@ -0,0 +1,13 @@
+package restbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBookServiceApplication.class, args);
+	}
+
+}
