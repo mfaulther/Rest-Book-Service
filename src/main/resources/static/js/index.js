ce1d3045ee79522bdaf2a9f6535d22611c194e4d
Vadim Khizhin <martin.faulther@yandex.ru>
2020-11-24 16:58:38 +0300

    Update Vue.js client

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,27 +1,28 @@
 bookApi = Vue.resource("/api/books");
 
-Vue.component("book-list", {
-    props: ["books"],
-    template: "<ul> <book-item v-for='book in books' v-bind:book='book'> </book-item> </ul>",
+Vue.component('book-list', {
+    props: ['books'],
+    template: '<ul> <book-item v-for="book in books" v-bind:book="book"> {{ book.author}} : {{ book.title }} </book-item> </ul>'
 });
 
 
-Vue.component("book-item", {
-    props: ["book"],
-    template: "<li v-bind:key='book.id'> <b> {{ book.author }}: </b> {{ book.title }} </li>"
+Vue.component('book-item', {
+
+    props: ['book'],
+    template: '<li> {{ book.author }} : {{ book.title }} </li>'
+
 });
 
 app = new Vue({
 
     el: "#app",
-    template:'<book-list v-bind:books="books"/>',
     data: {
-        books: [],
+        books: []
     },
-    created: function() {
+    created: function () {
         bookApi.get().then(result =>
-                result.body.forEach(book => this.books.push(book))
+            result.body.forEach(book => this.books.push(book))
         )
     }
-});
 
+});
